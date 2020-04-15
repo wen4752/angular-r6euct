defe340da6977a69e59e486373499c65548538c7
wen4752 <wenaudi74@gmail.com>
2020-04-15 10:24:01 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Wen Fernando';
+  name = 'Wen';
   itembuah = [{
     nama:'Kelengkeng',
     harga: 10000
@@ -15,4 +15,19 @@ export class AppComponent  {
     nama : 'Jeruk',
     harga: 15000
   }];
+
+  itemArr = ['aa','bb','vv']
+
+  show:boolean=false
+
+  user=""
+
+  pesan='';
+  klikButton(){
+    this.pesan="Tombol ditekan"
+  }
+  output=0
+  hitung(a,b){
+    this.output = Math.pow(a,b)
+  }
 }
