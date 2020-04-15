a245700bdd70ec5c35da7f66348d81e1590dccd3
wen4752 <wenaudi74@gmail.com>
2020-04-15 11:18:37 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,31 +3,62 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Wen';
   itembuah = [{
-    nama:'Kelengkeng',
+    nama: 'Kelengkeng',
     harga: 10000
-  }, 
+  },
   {
-    nama : 'Jeruk',
+    nama: 'Jeruk',
     harga: 15000
   }];
 
-  itemArr = ['aa','bb','vv']
+  itemArr = ['aa', 'bb', 'vv']
 
-  show:boolean=false
+  show: boolean = false
 
-  user=""
+  user = ""
 
-  pesan='';
-  klikButton(){
-    this.pesan="Tombol ditekan"
+  pesan = '';
+
+  klikButton() {
+    this.pesan = "Tombol ditekan"
   }
-  output=0
-  hitung(a,b){
-    this.output = Math.pow(a,b)
+  output
+  pola = ""
+  bentukPola = ""
+  hitung(a, b) {
+    this.output = Math.pow(a, b)
+    var max
+    if (a > b)
+      max = a
+    else
+      max = b
+    if (Math.pow(a, b) % 2 == 0) {
+      this.pola = "B"
+      for (var i = 0; i < Math.pow(a, b) - 2; i++) {
+        for (var j = 0; j < max + i; j++) {
+          this.bentukPola += '*'
+        }
+        this.bentukPola += '<br>'
+      }
+    }
+    else {
+      this.bentukPola=""
+      this.pola = "A"
+      for(var i = parseInt(max);i>0;i++){
+        
+      }
+    }
+
+
+
+
   }
+
+
+
 }
